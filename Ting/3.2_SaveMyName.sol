0b9878587af469936062288d4dd11cff188189fc
TingsWorld <sytsyt188@gmail.com>
2026-03-02 20:56:10 +0800

    upload

--- Ting/3.2_SaveMyName.sol
+++ Ting/3.2_SaveMyName.sol
@@ -6,13 +6,18 @@ contract SaveMyName{
 
     string name;
     string bio;
+    uint256 age;
+    string career;
 
-    function add (string memory _name,string memory _bio)public{
+
+    function add (string memory _name,string memory _bio,uint256  _age,string memory _career)public{
         name = _name;
         bio = _bio;
+        age = _age;
+        career = _career;
     }
 
-    function retrieve() public view returns(string memory,string memory){
-        return (name,bio);
+    function retrieve() public view returns(string memory,string memory,uint256 ,string memory){
+        return (name,bio,age,career);
     }
 }
