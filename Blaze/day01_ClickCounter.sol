f3687e39fb1893e06714c8a46026314d64c0f736
blaze1128 <1954767436@qq.com>
2026-03-03 18:05:57 +0800

    day01作业

--- Blaze/day01_ClickCounter.sol
+++ Blaze/day01_ClickCounter.sol
@@ -9,4 +9,27 @@ contract ClickCounter {
     function click() public {
         counter++;
     }
+
+    // 函数 - 计数器重置为0
+    function reset() public {
+        counter = 0;
+    }
+
+    //使计数器减1
+    function decrease() public {
+        require(counter > 0, "Counter is already at 0");
+        counter -= 1;
+    }
+
+    //返回当前计数
+    function getCounter() public view returns (uint256) {
+        return counter;
+    }
+    
+    //一次增加多次
+    function clickMultiple(uint256 times) public {
+        counter += times;
+    }
+
+
 }
