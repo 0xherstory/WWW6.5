66a9e5cf0f27c814939b056d6aabdd5d0cc0b22e
qiqiao0605 <qiqiaoO605@Outlook.com>
2026-03-03 18:30:17 +0800

    Remove ClickCounter contract implementation

--- day1_clickcounter.sol
+++ day1_clickcounter.sol
@@ -1,13 +1,1 @@
-// SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
-
-contract ClickCounter {
-
-    uint256 public counter ;
-
-    function click() public 
-    {
-        counter ++;
-    } 
-}
