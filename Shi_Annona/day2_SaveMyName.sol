3258ec02fc9341d1fcfa414213cfd02cfc4fdf3c
shiannona-cloud <shiannona@gmail.com>
2026-03-03 10:28:20 +0000

    day2_SaveMyName

--- Shi_Annona/day2_SaveMyName.sol
+++ Shi_Annona/day2_SaveMyName.sol
@@ -4,22 +4,22 @@ pragma solidity ^0.8.0;
 contract SaveMyName{
 
     string name;
-    string bios;
+    string job; 
 
-    function add(string memory _name, string memory _bios) public {
+    function add(string memory _name, string memory _job) public {
         name = _name;
-        bios = _bios;
+        job = _job;
     }
 
     function retrieve() public view returns (string memory, string memory)
     {
-        return(name,bios);
+        return(name,job);
     } 
 
 //不建议这么写
-    function AddAndRetrieve(string memory _name, string memory _bios) public returns(string memory, string memory) {
+    function AddAndRetrieve(string memory _name, string memory _job) public returns(string memory, string memory) {
         name = _name;
-        bios = _bios;
-        return(name,bios);//它会执行，但是在用户界面没显示，执行了就消耗gas，所以不要这么写
+        job = _job;
+        return(name,job);//它会执行，但是在用户界面没显示，执行了就消耗gas，所以不要这么写
     }
 }
