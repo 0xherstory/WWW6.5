138f732ac36c1554342134ae138643b9a6d1618e
AmorChen-01 <ad_06906@qq.com>
2026-03-04 20:55:33 +0800

    feat: add day2_SaveMyName.sol

---
+++ AmorChen/day2_SaveMyName.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.0;
+
+contract SaveMyName{
+    string name;
+    string bio;
+
+    function add(string memory_name, string memory_bio) public{
+        name = name_;
+        bio = bio_;
+    }
+
+    function retrieve() public view returns(string memory, string memory){
+        return (name, bio);
+    }
+}
