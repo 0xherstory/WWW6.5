455ab2662c7477eba2e09041e8329f8289deb3f5
cwj526 <caowenjia1526@gmail.com>
2026-03-07 21:14:48 +0800

    补交作业

---
+++ jiajia/day1.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+
+
+contract ClickCounter_jiajia{
+    
+    uint256 public herstory ; 
+
+    function click() public 
+    {
+        herstory ++; 
+    } 
+}
