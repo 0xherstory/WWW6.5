cf97b9142a329f35218702939fb6b341ba3a9ae8
foresee1990 <foresee1990@gmail.com>
2026-03-08 16:32:23 +0800

    Rowan尝试闯关 Day01-Day07 作业

--- Rowan/Day02_SaveMyName.sol
+++ Rowan/Day02_SaveMyName.sol
@@ -1,4 +1,5 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier:MIT
+
 pragma solidity ^0.8.0;
 
 contract SaveMyName{
@@ -6,7 +7,7 @@ contract SaveMyName{
   string name;
   string bio;
 
-  function add (string memory _name, string memory _bio ) public {
+  function add (string memory _name, string memory _bio )public {
     name = _name;
     bio = _bio;
   }
@@ -14,4 +15,5 @@ contract SaveMyName{
   function retrieve() public view returns(string memory, string memory){
     return (name,bio);
   }
+
 }
