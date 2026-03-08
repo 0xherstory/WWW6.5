d0e408b5d3987eb29e0d9866edddac18901c9491
Sarah07de27 <sarah2727hs@gmail.com>
2026-03-08 23:59:58 +0800

    Create ClickCounter.sol

---
+++ Sarah/day1/ClickCounter.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.0;
+
+contract ClickCounter{
+
+    uint256 public counter ;
+
+    function click() public 
+    {
+        counter ++;
+    }
+}
+// 重置计数器到0
+    function reset() public {
+        counter = 0;
+    }
+}
