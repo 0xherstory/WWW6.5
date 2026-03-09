a82d422d0f2f87f7f6753dc5798ae285b78301fc
sniglet0921 <sniglet0921@gmail.com>
2026-03-09 15:52:02 +0000

    9

--- Sniglet/day9-Calculator.sol
+++ Sniglet/day9-Calculator.sol
@@ -1,24 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-contract scientificCalculator {
-    function power (uint256 base,uint256 exponent) public pure returns (uint256){
-        uint256 result =1;
-        for (uint256 i=0 ;i < exponent; i ++) {
-            result *=base;
-        }
-        return result;
+
+
+import "./ScientificCalculator.sol";
+
+contract Calculator{
+    address public owner;
+    address public scientificCalculatorAddress;
+
+    constructor (){
+        owner = msg.sender;
     }
 
-function squareRoot (uint256 number) public pure returns (uint256){
-    if(number==0) return 0;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner");
+        _;}
 
-    uint256 z =(number + 1) /2;
-    uint256 y =number;
-    while (z < y) {
-       y = z;
-       z = (number / z + z) / 2;
-        }
-        return y;
+    function setscientificCalculator(address _address) public onlyOwner{
+        setscientificCalculatorAddress= _address;
+
+    }
+
+    function power(uint256 base , uint256 exponent) public view returns(uint256){
+        require(scientificCalculatorAddress != address(0),"calculator not set");
+    ScientificCalculator scientificCalc = ScientificCalculator(scientificCalculatorAddress);
+        return scientificCalc.power(base, exponent);
+    }
+   
 
 }
-}
+
+
+
+
