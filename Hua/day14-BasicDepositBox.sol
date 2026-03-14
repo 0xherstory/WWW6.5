13778c1295d74a25563d663b3104dd89bb8024c2
Hua <wangzihua5107@gmail.com>
2026-03-14 22:03:41 +0800

    Update day14-BasicDepositBox.sol

--- Hua/day14-BasicDepositBox.sol
+++ Hua/day14-BasicDepositBox.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0; 
 
-import{contract BasicDepositBox} from "day14-BaseDepositBox.sol";
+import "./day14-BaseDepositBox.sol";
 
 contract BasicDepositBox is BaseDepositBox{
 
