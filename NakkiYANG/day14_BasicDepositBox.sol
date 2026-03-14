61ac3138a982d07d6f49984c6c6db1b7f54303f4
NakkiYANG <NakkiYANG@protonmail.com>
2026-03-14 17:42:26 +0000

    modified day14 again

--- NakkiYANG/day14_BasicDepositBox.sol
+++ NakkiYANG/day14_BasicDepositBox.sol
@@ -1,4 +1,6 @@
-// SPDX-License-Identifier: MITpragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
 
 import "./day14_BaseDepositBox.sol";
 
--- NakkiYANG/day14_VaultManager.sol
+++ NakkiYANG/day14_VaultManager.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
+
 
 import "./day14_IDepositBox.sol";
 import "./day14_BasicDepositBox.sol";
