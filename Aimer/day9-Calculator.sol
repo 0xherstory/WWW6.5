28d12ab50fa9f7741e02a9dda227948e7cfcbd45
TopAimer <141472557+AimerMike@users.noreply.github.com>
2026-03-14 22:39:18 +0800

    Update import statement for ScientificCalculator

--- Aimer/day9-Calculator.sol
+++ Aimer/day9-Calculator.sol
@@ -3,7 +3,7 @@ day9-ScientificCalculator.sol
 
 pragma solidity ^0.8.0;
 
-import "./day9-ScientificCalculator.sol";
+import {ScientificCalculator} from "./day9-ScientificCalculator.sol";
 
 contract Calculator{
 
