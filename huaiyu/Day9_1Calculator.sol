afe414c6b14b05345e898949cd28714e1e5221c7
huaiyu <158258632+huaiyuuni@users.noreply.github.com>
2026-03-14 10:36:17 +0000

    Day9作业修改

--- huaiyu/Day9_1Calculator.sol
+++ huaiyu/Day9_1Calculator.sol
@@ -1,13 +1,13 @@
 //SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
-//加入scientificCalculator的代码
+//加入ScientificCalculator的代码
 import "./ScientificCalculator.sol";
 
 contract Calculator{
 //记录 ScientificCalculator 合约的地址，所有人都能看到
     address public owner;
-    address public ScientificCalculatorAddress;
+    address public scientificCalculatorAddress;
 //msg.sender部署这个合约的人，owner = msg.sender 把部署者设为所有者
     constructor(){
         owner = msg.sender;
@@ -15,14 +15,14 @@ contract Calculator{
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Only owner can do this action");
-        _;
+         _; 
     }
 
     function setScientificCalculator(address _address)public onlyOwner{
-        ScientificCalculatorAddress = _address;
-    }
+        scientificCalculatorAddress = _address;
+        }
 
-    function add(uint256 a, uint256 b) public pure returns(uint256){
+    function add(uint256 a, uint256 b)public pure returns(uint256){
         uint256 result = a+b;
         return result;
     }
@@ -38,30 +38,31 @@ contract Calculator{
     }
 
     function divide(uint256 a, uint256 b)public pure returns(uint256){
-        require(b!= 0,"Cannot divide by zero");
+        require(b!= 0, "Cannot divide by zero");
         uint256 result = a/b;
         return result;
     }
 // base是底数，exponent是指数
-    function calculatepower(uint256 base, uint256 exponent)public view returns(uint256){
+    function calculatePower(uint256 base, uint256 exponent)public view returns(uint256){
 
-    ScientificCalculator ScientificCalc = ScientificCalculator(ScientificCalculatorAddress);
+    ScientificCalculator scientificCalc = ScientificCalculator(scientificCalculatorAddress);
 
-    // external call
-        uint256 result = scientificCalc.power(base, exponent);
+    //external call 
+    uint256 result = scientificCalc.power(base, exponent);
 
     return result;
 
-    }
+}
 
     function calculateSquareRoot(uint256 number)public returns (uint256){
-        require(number >= 0, "Cannot calculate square root of negative number");
+        require(number >= 0 , "Cannot calculate square root of negative nmber");
 
-        bytes memory date = abi.encodeWithSignature("squareRoot(uint256)", number);
+        bytes memory data = abi.encodeWithSignature("squareRoot(int256)", number);
         (bool success, bytes memory returnData) = scientificCalculatorAddress.call(data);
+        require(success, "External call failed");
         uint256 result = abi.decode(returnData, (uint256));
         return result;
     }
 
-
+    
 }
