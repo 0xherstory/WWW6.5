7280dd9594cf51ace41fcfcc4a648030db956a8d
mugu <60588846+yinmincc@users.noreply.github.com>
2026-03-15 15:02:41 +0100

    Create day14_BaseDepositBox.sol

---
+++ Kathy/day14_BaseDepositBox.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./day14_IDepositBox.sol";
+
+abstract contract BaseDepositBox is IDepositBox {
+    address private owner;
+    string private secret;
+    uint256 private depositTime;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event SecretStored(address indexed Owner);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the box owner.");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+        depositTime = block.timestamp;
+    }
+
+    function getOwner() public  view override returns (address) {
+        return owner;
+    }
+
+    function transferOwnership(address newOwner) external virtual override  onlyOwner {
+        require((newOwner != address(0), "New owner can not be zero address."));
+        emit OwnershipTransfeered(owner, newOwner);
+        owner = newOwner;
+    }
+
+    function storeSecret(string calldata _secret) external virtual override onlyOwner {
+        secret = _secret;
+        emit SecretStored(msg.sender);
+    }
+
+    function getSecret() public view virtual override onlyOwner returns (String memory) {
+        return secret;
+    }
+
+    function getDepositTime() external view  virtual  override  returns (uint256) {
+        return  depositTime;
+    }
+}
