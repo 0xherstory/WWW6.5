0c423fb1a2cbb45b54c21f9a502daee0beff2ee4
Lexi1128 <obrienmikayla5ycoe@gmail.com>
2026-03-15 14:49:35 +0000

    day13

--- Lexi1128/day13_PreOrderToken.sol
+++ Lexi1128/day13_PreOrderToken.sol
@@ -1,10 +1,9 @@
 //SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
+import "./day13_MyToken.sol";
 
-import "./day12_ERC20.sol";
-
-contract PreOrderToken is ERC20 {
+contract PreOrderToken is MyToken {
 
     uint256 public tokenPrice;
     uint256 public saleStartTime;
@@ -20,25 +19,25 @@ contract PreOrderToken is ERC20 {
     event SaleFinalized(uint256 totalRaised, uint256 totalTokensSold);
 
     constructor( 
-        uint256 _initialSupply,
+        uint256 _intitialSupply,
         uint256 _tokenPrice,
         uint256 _saleDurationInSeconds,
         uint256 _minPurchase,
         uint256 _maxPurchase,
         address _projectOwner
-    ) ERC20(_initialSupply) { 
+    )MyToken(_intitialSupply){
         tokenPrice = _tokenPrice;
         saleStartTime = block.timestamp;
         saleEndTime = block.timestamp + _saleDurationInSeconds;
         minPurchase = _minPurchase;
         maxPurchase = _maxPurchase;
         projectOwner = _projectOwner;
+    
 
-        _transfer(msg.sender, address(this), totalSupply());
-        initialTransferDone = true;
-    }
-
-    function isSaleActive() public view returns(bool){
+    _transfer(msg.sender, address(this), totalSupply);
+    initialTransferDone = true;
+}
+    function isSaleActive()public view returns(bool){
         return(!finalized && block.timestamp >= saleStartTime && block.timestamp <= saleEndTime);
     }
 
@@ -46,53 +45,60 @@ contract PreOrderToken is ERC20 {
         require(isSaleActive(), "Sale is not active");
         require(msg.value >= minPurchase, "Amount is below min purchase");
         require(msg.value <= maxPurchase, "Amount is above max purchase");
-        uint256 tokenAmount = (msg.value * 10**uint256(decimals())) / tokenPrice;
-        require(balanceOf(address(this)) >= tokenAmount, "Not enough tokens left for sale");
-        totalRaised += msg.value;
-        _transfer(address(this), msg.sender, tokenAmount);
+        uint256 tokenAmount = (msg.value * 10**uint256(decimals))/ tokenPrice;
+        require(balanceOf[address(this)] >= tokenAmount, "Not enough tokens left for sale");
+        totalRaised+= msg.value;
+        _transfer(address(this),msg.sender,tokenAmount);
         emit TokensPurchased(msg.sender, msg.value, tokenAmount);
+        
     }
 
-    function transfer(address _to, uint256 _value) public override returns(bool){
+    function transfer(address _to, uint256 _value)public override returns(bool){
         if(!finalized && msg.sender != address(this) && initialTransferDone){
-            revert("Tokens are locked until sale is finalized");
+            require(false, "Tokens are locked until sale is finalized");
         }
+
         return super.transfer(_to, _value);
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) public override returns(bool){
+    function transferFrom(address _from, address _to, uint256 _value)public override returns(bool){
         if(!finalized && _from != address(this)){
-            revert("Tokens are locked until sale is finalized");
+            require(false, "Tokens are locked until sale is finalized");
         }
         return super.transferFrom(_from, _to, _value);
     }
 
-    function finalizeSale() public {
+    function finalizeSale() public payable{
         require(msg.sender == projectOwner, "Only owner can call this function");
         require(!finalized,"Sale is already finalized");
-        require(block.timestamp > saleEndTime, "Sale not finished yet");
+        require (block.timestamp > saleEndTime, "Sale not finished yet");
         finalized = true;
-        uint256 tokensSold = totalSupply() - balanceOf(address(this));
-        (bool success,) = projectOwner.call{value: address(this).balance}("");
-        require(success, "Transfer failed");
+        uint256 tokensSold = totalSupply - balanceOf[address(this)];
+        (bool sucess,) = projectOwner.call{value:  address(this).balance}("");
+        require(sucess, "Transfer failed");
         emit SaleFinalized(totalRaised, tokensSold);
     }
 
-    function timeRemaining() public view returns(uint256){
+    function timeRemaining() public view  returns(uint256){
         if(block.timestamp >= saleEndTime){
             return 0;
         }
         return (saleEndTime - block.timestamp);
     }
 
-    function tokensAvailable() public view returns(uint256){
-        return balanceOf(address(this));
+    function tokensAvailable()public view returns(uint256){
+        return balanceOf[address(this)];
     }
 
     receive() external payable{
         buyTokens();
     }
-}
+    }
+
+
+
+
+
 
 
 
