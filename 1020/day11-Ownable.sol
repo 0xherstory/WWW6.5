c0b607e10c88254778ced6e211acf4224ca03124
1020-ops <liujunting_2020@163.com>
2026-03-15 22:19:52 +0800

    modified wrong part

--- 1020/day11-Ownable.sol
+++ 1020/day11-Ownable.sol
@@ -8,12 +8,12 @@ pragma solidity ^0.8.20;
 contract Ownable{
     address private owner;
 
-    evet OwnershipTransferred(
+    event OwnershipTransferred(
         address indexed previousOwner,
         address indexed newOwner);
     constructor(){
         owner=msg.sender;
-        emit OwnershioTransferred(address(0),msg.sender);
+        emit OwnershipTransferred(address(0),msg.sender);
     }
 
     modifier onlyOwner(){
@@ -25,10 +25,10 @@ contract Ownable{
         return owner;
     }
     
-    function transferOwnership(address _newOwner) public only Owener{
+    function transferOwnership(address _newOwner) public onlyOwner{
         require(_newOwner!=address(0),"Invalid address");
         address previous=owner;
         owner=_newOwner;
-        emit OwnershipTransferred(previous,owner);
+        emit OwnershipTransferred(previous,_newOwner);
     }
 }
