35a95d459bbc7eebec50b720404feac4542f6558
Sheryl0907 <Sheryl0907@gmail.com>
2026-03-15 12:19:01 +0800

    Add Day13 homework: PreOrderToken.sol

--- Sheryl/Day12_SimpleERC20.sol
+++ Sheryl/Day12_SimpleERC20.sol
@@ -1,14 +1,15 @@
+ 
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 contract SimpleERC20 {
-    string public name = "SimpleToken";
-    string public symbol = "SIM";
-    uint public decimals = 18;
-    uint public totalSupply;
+    string public name = "Web3 Compass";
+    string public symbol = "COM";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
 
-    mapping (address => uint) public balanceOf;
-    mapping (address => mapping (address =>uint)) public allowance;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
@@ -19,16 +20,15 @@ contract SimpleERC20 {
         emit Transfer(address(0), msg.sender, totalSupply);
     }
 
-    function _transfer(address _from, address _to, uint256 _value) internal {
-        require(_to != address(0), "Invalid address");
-        balanceOf[_from] -= _value;
-        balanceOf[_to] += _value;
-        emit Transfer(_from, _to, _value);
-    }
-
     function transfer(address _to, uint256 _value) public returns (bool) {
         require(balanceOf[msg.sender] >= _value, "Not enough balance");
         _transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
@@ -41,10 +41,10 @@ contract SimpleERC20 {
         return true;
     }
 
-    function approve(address _spender, uint256 _value) public returns (bool) {
-        allowance[msg.sender][_spender] = _value;
-        emit Approval(msg.sender, _spender, _value);
-        return true;
+    function _transfer(address _from, address _to, uint256 _value) internal {
+        require(_to != address(0), "Invalid address");
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value);
     }
-
 }
