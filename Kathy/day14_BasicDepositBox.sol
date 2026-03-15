d78f656ba6431d9d9bc607db741118b37ab7a285
mugu <60588846+yinmincc@users.noreply.github.com>
2026-03-15 15:23:13 +0100

    Update day14_BasicDepositBox.sol

--- Kathy/day14_BasicDepositBox.sol
+++ Kathy/day14_BasicDepositBox.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "./day14_BaseDepositBox.sol";
 
-contract BasicDepositBox is BaseDepostiBox {
+contract BasicDepositBox is BaseDepositBox {
     function getBoxType() external pure override returns (string memory) {
         return "basic";
     // external: 仅用于从合约外部调用； pure：不读写，只返回
