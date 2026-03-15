347387fa2544fdd4f466184828acef0cfbd9439f
FireCrazyBaby <yourhotbabyonfire@gmail.com>
2026-03-15 14:20:24 +0700

    Update day14_PremiumDepositBox.sol

--- fireCrazy/Day14_SafeDepositSystem/day14_PremiumDepositBox.sol
+++ fireCrazy/Day14_SafeDepositSystem/day14_PremiumDepositBox.sol
@@ -1,8 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./day14_BaseDepositBox.sol"; // 导入地基
+import "./day14_BaseDepositBox.sol";
 
 contract PremiumDepositBox is BaseDepositBox {
-    // ... 这里放 PremiumDepositBox 的代码 ...
+    string private metadata;
+    event MetadataUpdated(address indexed owner);
+
+    function getBoxType() external pure override returns (string memory) {
+        return "Premium";
+    }
+
+    function setMetadata(string calldata _metadata) external onlyOwner {
+        metadata = _metadata;
+        emit MetadataUpdated(msg.sender);
+    }
+
+    function getMetadata() external view onlyOwner returns (string memory) {
+        return metadata;
+    }
 }
