b005ca734a0fdb97cadece8a0c4b4eac3ff2e9c7
FireCrazyBaby <yourhotbabyonfire@gmail.com>
2026-03-15 13:25:15 +0700

    Create VaultManager.sol

---
+++ fireCrazy/Day14_SafeDepositSystem/VaultManager.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./IDepositBox.sol";
+import "./BasicDepositBox.sol";
+import "./PremiumDepositBox.sol";
+import "./TimeLockedDepositBox.sol";
+
+contract VaultManager {
+    // ... 这里放 VaultManager 的所有代码 ...
+}
