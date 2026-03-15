60121a715d2bc238a284337975159dff8c76f4e2
FireCrazyBaby <yourhotbabyonfire@gmail.com>
2026-03-15 14:00:10 +0700

    Update day14_BaseDepositBox.sol

--- fireCrazy/Day14_SafeDepositSystem/day14_BaseDepositBox.sol
+++ fireCrazy/Day14_SafeDepositSystem/day14_BaseDepositBox.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./IDepositBox.sol"; // 导入接口
+import "./day14_IDepositBox.sol"; // 导入接口
 
 abstract contract BaseDepositBox is IDepositBox {
     // ... 这里放 BaseDepositBox 的所有代码 ...
