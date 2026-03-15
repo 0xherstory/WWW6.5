b0f91d35867c16b574c16a2001988bacda5020f6
mugu <60588846+yinmincc@users.noreply.github.com>
2026-03-15 15:07:05 +0100

    Create day14_BasicDepositBox.sol

---
+++ Kathy/day14_BasicDepositBox.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./day14_BaseDepositBox.sol";
+
+contract BasicDepositBox is BaseDepostiBox {
+    function getBoxType() external pure override returns (string memory) {
+        return "basic";
+    // external: 仅用于从合约外部调用； pure：不读写，只返回
+    }
+}
