a72e8a39e2202cc50eb10959a8a280d70dcb4aa6
FireCrazyBaby <yourhotbabyonfire@gmail.com>
2026-03-15 14:01:27 +0700

    Update day14_VaultManager.sol

--- fireCrazy/Day14_SafeDepositSystem/day14_VaultManager.sol
+++ fireCrazy/Day14_SafeDepositSystem/day14_VaultManager.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.0;
 
 import "./day14_IDepositBox.sol";
-import "./BasicDepositBox.sol";
-import "./PremiumDepositBox.sol";
-import "./TimeLockedDepositBox.sol";
+import "./day14_BasicDepositBox.sol";
+import "./day14_PremiumDepositBox.sol";
+import "./day14_TimeLockedDepositBox.sol";
 
 contract VaultManager {
     // ... 这里放 VaultManager 的所有代码 ...
