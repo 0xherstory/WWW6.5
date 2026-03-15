51b83617c9c7db3d3fce33bd1dbf32ba79d32f92
FireCrazyBaby <yourhotbabyonfire@gmail.com>
2026-03-15 14:00:57 +0700

    Update day14_TimeLockedDepositBox.sol

--- fireCrazy/Day14_SafeDepositSystem/day14_TimeLockedDepositBox.sol
+++ fireCrazy/Day14_SafeDepositSystem/day14_TimeLockedDepositBox.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./BaseDepositBox.sol"; // 导入地基
+import "./day14_BaseDepositBox.sol"; // 导入地基
 
 contract TimeLockedDepositBox is BaseDepositBox {
     // ... 这里放 TimeLockedDepositBox 的代码 ...
