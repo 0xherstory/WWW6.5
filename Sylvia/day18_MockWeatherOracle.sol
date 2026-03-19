dd1b46e42174277e0ceef27df2767d0272b003be
sylvia-tso <1123819191c@gmail.com>
2026-03-19 18:01:48 +0800

    Update day18_MockWeatherOracle.sol

--- Sylvia/day18_MockWeatherOracle.sol
+++ Sylvia/day18_MockWeatherOracle.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./day18_AggregatorV3Interface.sol";
+import "./day11_Ownable.sol";
 
 contract MockWeatherOracle is AggregatorV3Interface, Ownable {
     uint8 private _decimals;
