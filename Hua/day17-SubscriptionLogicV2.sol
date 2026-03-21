bfb9540f6b99439afc13dd916a5e17883bebcb02
Hua <wangzihua5107@gmail.com>
2026-03-21 23:09:37 +0800

    Update day17-SubscriptionLogicV2.sol

--- Hua/day17-SubscriptionLogicV2.sol
+++ Hua/day17-SubscriptionLogicV2.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./day17_SubscriptionStorageLayout.sol";
+import "./day17-SubscriptionStorageLayout.sol";
 
 contract SubscriptionLogicV2 is SubscriptionStorageLayout {
 
