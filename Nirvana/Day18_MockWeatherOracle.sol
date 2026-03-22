d257b7601fdbffac34da6c4199f96b6c5ed15f51
0xDiaspora <nirvana.li1998@gmail.com>
2026-03-22 11:02:19 +0000

    Day

--- Nirvana/Day18_MockWeatherOracle.sol
+++ Nirvana/Day18_MockWeatherOracle.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.19;
-
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+// import "https://raw.githubusercontent.com/smartcontractkit/chainlink/refs/tags/contracts-v1.3.0/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Day17_MockWeatherOracle is AggregatorV3Interface, Ownable {
