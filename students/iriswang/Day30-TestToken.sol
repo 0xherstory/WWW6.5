e03c869776d921573d0c815c3eb6fc9119f4fb58
iris0502wang <iris0502wang@gmail.com>
2026-03-31 23:36:34 +0800

    Update Day30-TestToken.sol

--- students/iriswang/Day30-TestToken.sol
+++ students/iriswang/Day30-TestToken.sol
@@ -1,15 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./Day30-IERC20.sol";
 
-contract TestToken is ERC20 {
+contract TestToken is IERC20 {
 
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-        _mint(msg.sender, 1000000 * 10 ** 18); // 给部署者100万代币
+    string public name;
+    string public symbol;
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor(string memory _name, string memory _symbol) {
+        name = _name;
+        symbol = _symbol;
+        _mint(msg.sender, 1000000 * 10 ** 18);
     }
 
-    // 手动发币（测试用）
+    function transfer(address to, uint256 amount) external override returns (bool) {
+        require(balanceOf[msg.sender] >= amount, "Not enough balance");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external override returns (bool) {
+        require(balanceOf[from] >= amount, "Not enough balance");
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        return true;
+    }
+
+    function _mint(address to, uint256 amount) internal {
+        balanceOf[to] += amount;
+        totalSupply += amount;
+    }
+
     function mint(address to, uint256 amount) external {
         _mint(to, amount);
     }
