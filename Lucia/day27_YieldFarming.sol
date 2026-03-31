f7c9a6274ff07a2f48007392009ea037eb7d097a
futureclover87 <sljclover@hotmail.com>
2026-03-31 13:03:41 +0800

    Final with openzeppelin update

--- Lucia/day27_YieldFarming.sol
+++ Lucia/day27_YieldFarming.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/math/SafeCast.sol"; // 如果需要使用 SafeCast
 //安全转换数字，防止溢出或者截断值
 
